9ac93a6e30a2c983958fee8c7292b2c696d40cee
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-08 11:03:02 -0300

    Question 1541

---
+++ Beginner/bee 1541*/bee1541.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int x, y, percentage;
+    while(1){
+        scanf("%d", &x);
+        if(x == 0){
+            break;
+        }
+        scanf("%d %d", &y, &percentage);
+        
+        int value = sqrt((100 * x * y)/percentage);
+        printf("%d\n", value);
+    }
+    return 0;
+}
