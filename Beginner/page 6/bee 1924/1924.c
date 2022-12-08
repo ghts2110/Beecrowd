1d3121f3ee35b422d75c10a2a2b2bb13d4dab9bc
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-08 20:33:43 -0300

    Question 1924

---
+++ Beginner/page 6/bee 1924/1924.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    char name[30];
+    scanf("%d", &t);
+    while(scanf("%s", name) != EOF){
+    }
+    printf("Ciencia da Computacao\n")
+    return 0;
+}
