1608604c6aaa944cf436628ea04ac816333a44c0
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-08 11:03:23 -0300

    Question 1541

---
+++ Beginner/bee 1864*/bee1864.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char array[34] = {"LIFE IS NOT A PROBLEM TO BE SOLVED"};
+    int t, i;
+    scanf("%d", &t);
+    for(i = 0; i != t; i++){
+        printf("%c", array[i]);
+    }printf("\n");
+    return 0;
+}
