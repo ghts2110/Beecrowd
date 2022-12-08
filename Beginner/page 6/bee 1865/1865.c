b7b02932d3ddaa9bd57d4678ce46aeca1755a5e8
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-08 18:22:21 -0300

    Question 1865

---
+++ Beginner/page 6/bee 1865/1865.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int t, power;
+    char name[7];
+    scanf("%d", &t);
+    while(t--){
+        scanf("%s %d", name, &power);
+        if(strcmp(name, "Thor") == 0){
+            printf("Y\n");
+        }else{
+            printf("N\n");
+        }
+    }
+    return 0;
+}
