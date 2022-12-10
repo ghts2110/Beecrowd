1d8249194eaa06b4f2c05f9ccbc940f2fffc5705
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-10 13:35:51 -0300

    Question 1973

---
+++ Beginner/page 6/bee 1973/1973.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    long long int t, i, planet = 0;
+    scanf("%lld", &t);
+    long long int array[t];
+    for(i = 0; i != t; i++){
+        scanf("%lld", &array[i]);
+    }
+    
+    long long int index = 0;
+    while(index != -1 && index != t){
+        if(array[index] % 2 == 0){
+            if(array[index] != 0){
+                array[index]--;
+            }
+            index--;
+        }else{
+            array[index]--;
+            index++;
+        }
+
+        if(index > planet){
+            planet++;
+        }
+    }
+    if(index == -1){
+        planet++;
+    }
+
+    long long int cont = 0;
+    for(i = 0; i != t; i++){
+        cont += array[i];
+    }
+    printf("%lld %lld\n",planet, cont);
+    return 0;
+}
