b95754dc5c9a1c47756ff043bd6db2fb4b3fb80a
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-10 11:03:15 -0300

    Question 1963

---
+++ Beginner/page 6/bee 1962*/1962.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int year;
+        scanf("%d", &year);
+        if(year <= 2014){
+            printf("%d D.C.\n", 2015 - year);
+        }else{
+            printf("%d A.C.\n", year - 2014);
+        }
+    }
+    return 0;
+}
