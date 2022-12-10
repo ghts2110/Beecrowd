aa20758b54404957447e02a874a704c41c200233
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-10 18:06:36 -0300

    Question 2006

---
+++ Beginner/page 6/bee 2028/2028.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int t, cont = 0;
+    while(scanf("%d", &t) != EOF){
+        int i, num = 0;
+        for(i = 0; i != t + 1; i++){
+            num += i;
+        }
+        cont++;
+        if(t == 0){
+            printf("Caso %d: %d numero\n", cont, num + 1);
+        }else{
+            printf("Caso %d: %d numeros\n", cont, num + 1);
+        }
+        int SequenceP = 0;
+        if(SequenceP == 0){
+            printf("%d", SequenceP);
+            SequenceP++;
+        }
+        while(t--){
+            for(i = 0; i != SequenceP; i++){
+                printf(" %d", SequenceP);
+            }
+            SequenceP++;
+        }
+        printf("\n\n");
+    }
+}
