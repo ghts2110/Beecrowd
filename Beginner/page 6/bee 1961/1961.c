e1d88e598bd969e453d026d61fdaf189babf25d1
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-09 22:21:12 -0300

    Question 1961

---
+++ Beginner/page 6/bee 1961/1961.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int P, t, win = 0, fist, last;
+    scanf("%d %d", &P, &t);
+
+    scanf("%d", &fist);
+    t--;
+    while(t--){ 
+        scanf("%d", &last);
+        if(fist + P < last || abs(fist - last) > P){
+            win = 1;
+        }
+
+        fist = last;
+    }
+
+    if(win == 0){
+        printf("YOU WIN\n");
+    }else{
+        printf("GAME OVER\n");
+    }
+    return 0;
+}
