0155ef9fffd9450fe65b626760cc694283480a1c
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-10 11:27:17 -0300

    Question 1963

---
+++ Beginner/page 6/bee 1963/1963.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    float new, old, value;
+    scanf("%f %f", &new, &old);
+    value = (old - new)/new;
+    printf("%.2f", value * 100);
+    char c = '%';
+    printf("%c\n", c);
+    return 0;
+}
