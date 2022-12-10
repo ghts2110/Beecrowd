08f833fa07f681523f11fcde9bdbe7690d352187
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-10 15:12:58 -0300

    Question 1985

---
+++ Beginner/page 6/bee 1985*/1985.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    float value = 0;
+    scanf("%d", &t);
+    while(t--){
+        float quantidade, cust;
+        int cod;
+        scanf("%d %f", &cod, &quantidade);
+        cust = cod % 10 + 0.50;
+        value += cust * quantidade;
+    }
+    printf("%.2f\n", value);
+    return 0;
+}
