6cb88b944e8b8fdedc9df074dbd7390229cf000c
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-11 12:21:22 -0300

    Question 2057

---
+++ Beginner/page 6/bee 2057*/2057.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int hora, tempo, fuso, final;
+    scanf("%d %d %d", &hora, &tempo, &fuso);
+    final = hora + tempo + fuso;
+    if(final >= 24){
+        final -= 24;
+    }else if(final < 0){
+        final += 24;
+    }
+
+    printf("%d\n", final);
+    return 0;
+}
