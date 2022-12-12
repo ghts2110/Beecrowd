b3fe5974466f64c65c2f56845e122a9a1d1367aa
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-11 23:04:37 -0300

    Question 2061

---
+++ Beginner/page 7/bee 2061*/2061.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int abas, t;
+    scanf("%d %d", &abas, &t);
+    while(t--){
+        char cod[7];
+        scanf("%s", cod);
+        if(cod[0] == 'f'){
+            abas++;
+        }else{
+            abas--;
+        }
+        if(abas < 0){
+            abas = 0;
+        }
+    }
+    printf("%d\n", abas);
+    return 0;
+}
