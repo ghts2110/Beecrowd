dedb966eac02971e7cf9290fb4982ba84f225cd2
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-13 13:00:34 -0300

    Question 2126

---
+++ Beginner/page 7/bee 2126/2126.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+    char n1[11] = "-----------", n2[33];
+    int caso = 0;
+    while(scanf("%s %s", n1, n2) != EOF){
+        caso++;
+        int i, j, cont = 0, inicio = 0, equal = 0, c, index, pos = 0;
+        for(i = 0; i != 11; i++){
+            if(n1[i] == '-'){
+                break;
+            }
+            cont++;
+        }
+        cont -= 2;
+
+        for(i = cont; i != 33; i++){
+            index = c = 0;
+            for(j = inicio; j != i + 1; j++){
+                if(n1[index] == n2[j]){
+                    index++;
+                    c++;
+                }
+            }
+            if(c == cont + 1){
+                pos = i - (cont - 1);
+                equal++;
+            }
+            inicio++;
+        }
+        
+        printf("Caso #%d:\n", caso);
+        if(equal == 0){
+            printf("Nao existe subsequencia\n");
+        }else{
+            printf("Qtd.Subsequencias: %d\n", equal);
+            printf("Pos: %d\n", pos);
+        }
+        printf("\n");
+
+        for(i = 0; i != 11; i++){
+            n1[i] = '-';
+        }
+    }
+    return 0;
+}
