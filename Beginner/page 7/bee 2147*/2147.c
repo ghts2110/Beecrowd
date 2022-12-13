fbc924e78b27250a30f3b2e260ae95ec94f8f69f
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-13 19:54:21 -0300

    Question 2152

---
+++ Beginner/page 7/bee 2147*/2147.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    char galopeira[10000];
+    scanf("%d", &t);
+    while(t--){
+
+        int i;
+        for(i = 0; i != 10000; i++){
+            galopeira[i]='-';
+        }
+
+        scanf("%s", galopeira);
+        float cont = 0;
+
+        for(i = 0; i != 10000; i++){
+            if(galopeira[i] == 'e'){
+                cont += 0.01;
+            }
+        }
+
+        printf("%.2f\n", cont + 0.08);
+
+    }
+    return 0;
+}
---
+++ Beginner/page 7/bee 2152*/2152.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int h, m, o;
+        scanf("%d %d %d", &h, &m, &o);
+
+        if(h < 10){
+            printf("0%d:", h);
+        }else{
+            printf("%d:", h);
+        }
+
+        if(m < 10){
+            printf("0%d - ", m);
+        }else{
+            printf("%d - ", m);
+        }
+
+        if(o == 1){
+            printf("A porta abriu!\n");
+        }else{
+            printf("A porta fechou!\n");
+        }
+    }
+    return 0;
+}
