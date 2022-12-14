7907d2ca9f11b49b30d90516e93a430d47448746
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-14 19:43:51 -0300

    Question 2160

---
+++ Beginner/page 7/bee 2160*/2160.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    char name[500] = "------------------------------------------------------------------------------------------";
+    int t = 2;
+
+    while(t--){
+        fgets(name, 500, stdin);
+        if(name[82] != '-'){
+            printf("NO\n");
+            break;
+        }else{
+            printf("YES\n");
+            break;
+        }
+    }
+    return 0;
+}
