2dfba204df7830fac5b95efab1e2c804d638358b
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-15 00:18:10 -0300

    Question 2164

---
+++ Beginner/page 7/bee 2164*/2164.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int n;
+    double value;
+    scanf("%d", &n);
+    
+    double raiz5 = sqrt(5);
+    value = (pow((1+raiz5)/2, n) - pow((1 - raiz5)/2, n))/raiz5;
+    printf("%.1lf\n", value);
+    return 0;
+}
