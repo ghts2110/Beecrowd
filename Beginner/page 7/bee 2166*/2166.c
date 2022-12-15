25ef97d0636add4df5506ab675c932489ebc77f0
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-15 00:39:05 -0300

    Question 2166

---
+++ Beginner/page 7/bee 2166*/2166.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int t, i;  
+    scanf("%d", &t);
+
+    double x = 2;
+    for(i = 0; i != t; i++){
+        x = 1/x;
+        if(i != t - 1){
+            x += 2;
+        }
+    }
+
+    x++;
+
+    if(t == 0){
+        x = 1;
+    }
+
+    printf("%.10lf\n", x);
+    return 0;
+}
