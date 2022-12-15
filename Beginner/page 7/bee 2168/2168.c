ff3924daad73e8407f88c1481e36b7e6c410bf90
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-15 17:59:24 -0300

    Question 2168

---
+++ Beginner/page 7/bee 2168/2168.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    int t, c;
+    scanf("%d", &t);
+
+    int mat[t + 1][t + 1];
+
+    int i,j;
+    for(i = 0; i != t + 1; i++){
+        for(j = 0; j != t + 1; j++){
+            scanf("%d", &c);
+            mat[i][j] = c;
+        }
+    }
+
+    for(i = 0; i != t; i++){
+        for(j = 0; j != t; j++){
+            int cont = 0;
+            if(mat[i][j] == 1){
+                cont++;
+            }
+            if(mat[i][j + 1] == 1){
+                cont++;
+            }
+            if(mat[i + 1][j] == 1){
+                cont++;
+            }
+            if(mat[i + 1][j + 1] == 1){
+                cont++;
+            }
+
+            if(cont >= 2){
+                printf("S");
+            }else{
+                printf("U");
+            }
+        }printf("\n");
+    }
+
+    return 0;
+}
