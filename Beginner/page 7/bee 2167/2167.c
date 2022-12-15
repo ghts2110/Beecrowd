79bd20bdc73cb93997b0f6a6ef24fb9ef83997a1
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-15 17:03:49 -0300

    Question 2167

---
+++ Beginner/page 7/bee 2167/2167.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int t, num, lastnum, cont = 2, value = 0, f = 0; 
+    scanf("%d", &t);
+    
+    scanf("%d", &lastnum);
+    t--;
+
+    while(t--){
+        scanf("%d", &num);
+        if(num < lastnum && f == 0){
+            value = cont;
+            f = 1;
+        }else{
+            cont++;
+            lastnum = num;
+        }
+    }
+
+
+
+    return 0;
+}
