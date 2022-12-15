2bbd409eefb9d36a270d98f2c0e7b3d4e882b494
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-15 19:50:41 -0300

    Question 2172

---
+++ Beginner/page 7/bee 2172/2172.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int x, xp;
+    while(1){
+        scanf("%d %d", &x, &xp);
+        if(x == 0 && xp == 0){
+            break;
+        }
+
+        printf("%d\n", x * xp);
+    }
+    return 0;
+}
