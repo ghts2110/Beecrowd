fe45b47c756c8d1ac979470176dc20eb295ca2a5
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-16 12:01:42 -0300

    Question 2221

---
+++ Beginner/page 7/bee 2221/2221.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int b, a1, d1, l1, a2, d2, l2, power1, power2;
+        scanf("%d %d %d %d %d %d %d", &b, &a1, &d1, &l1, &a2, &d2, &l2);
+        power1 = (a1 + d1)/2;
+        power2 = (a2 + d2)/2;
+
+        if(l1 % 2 == 0){
+            power1 += b;
+        }
+        if(l2 % 2 == 0){
+            power2 += b;
+        }
+
+        if(power1 > power2){
+            printf("Dabriel\n");
+        }else if(power1 < power2){
+            printf("Guarte\n");
+        }else{
+            printf("Empate\n");
+        }
+
+    }
+    return 0;
+}
