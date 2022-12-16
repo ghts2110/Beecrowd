9fa1d9126523c8b20a0dfe50206aa0c12c7e0728
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-16 18:42:44 -0300

    Question 2311

---
+++ Beginner/page 8/bee 2311*/2311.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+int main(){
+    int t, t2, i;
+    float point, d, n, max, min, cont;
+    char name[100];
+    scanf("%d", &t);
+
+    while(t--){
+        t2 = 7, max = 0, min = 100, cont = 0;
+        scanf("%s", name);
+        scanf("%f", &d);
+
+        while(t2--){
+            scanf("%f", &n);
+            if(n  > max){
+                max = n;
+            }
+            if(n < min){
+                min = n;
+            }
+
+            cont += n;
+        }
+            
+        cont -= max + min;
+        
+        point = d * cont;
+        printf("%s %.2f\n", name, point);
+    }
+    return 0;
+}
