31bffb5fab018ce6e850120f6fa38cc674b51193
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-17 15:45:32 -0300

    Question 2334

---
+++ Beginner/page 8/bee 2334*/2334.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    while(1){
+        long double p;
+        scanf("%LF", &p);
+
+        if(p == -1){
+            break;
+        }
+
+        if(p == 0){
+            printf("0\n");
+        }else{
+            p--;
+            printf("%.LF\n", p);
+                    
+        }
+    }
+    return 0;
+}
+
