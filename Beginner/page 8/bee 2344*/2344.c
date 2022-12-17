aeaebd13c271a5d0941ea59e705f5acc69be79d6
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-17 17:08:48 -0300

    Question 2344

---
+++ Beginner/page 8/bee 2344*/2344.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+
+    if(num < 1){
+        printf("E\n");
+    }else if(num < 36){
+        printf("D\n");
+    }else if(num < 61){
+        printf("C\n");
+    }else if(num < 86){
+        printf("B\n");
+    }else{
+        printf("A\n");
+    }
+    return 0;
+}
