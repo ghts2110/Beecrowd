95275fd2140352050cca765e7488d3e3ab1df806
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-17 17:13:22 -0300

    Question 2483

---
+++ Beginner/page 8/bee 2483/2483.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+
+    printf("Feliz nat");
+    int i;
+    for(i = 0; i != num; i++){
+        printf("a");
+    }
+    printf("l!\n");
+    return 0;
+}
