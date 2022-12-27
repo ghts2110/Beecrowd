89780e0d9f9d07de8e66394197f8e05c97e3157d
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-27 00:12:01 -0300

    Question 2510

---
+++ Beginner/2510.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        char name[25];
+        scanf("%s", name);
+        printf("Y\n");
+    }
+
+    return 0;
+}
