6bdf264d16c95b744b721459a179a2277ac577f1
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-28 19:27:44 -0300

    Question 2533

---
+++ Beginner/2533.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    while(scanf("%d", &t) != EOF){
+        int i = t;
+        double n, c, pro = 0, car = 0;
+        while(i--){
+            scanf("%lf %lf", &n, &c);
+            pro += n * c;
+            car += c;
+        }
+        
+        car *= 100;
+
+        double value = pro/car;
+        printf("%.4lf\n", value);
+    }
+    return 0;
+}
