a2be69a9fb24254d134aa1e9f4ef7b0ac6c55f5b
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-28 22:54:09 -0300

    Question 2540

---
+++ Beginner/2540.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    float t;
+
+    while(scanf("%f", &t) != EOF){
+        int cont = 0;
+        float min = (2*t)/3; 
+
+        while(t--){
+            int n;
+            scanf("%d", &n);
+            cont += n;
+        }
+
+        if(cont >= min){
+            printf("impeachment\n");
+
+        }else{
+            printf("acusacao arquivada\n");
+        }
+    }
+    return 0;
+}
