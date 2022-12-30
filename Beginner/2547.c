e3ce72d879bc9a86bf985029b9f53352223fc745
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-30 20:11:55 -0300

    Question 2547

---
+++ Beginner/2547.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n, max, min;
+    while(scanf("%d %d %d", &n, &min, &max) != EOF){
+        int cont = 0;
+        
+        while(n--){
+            int h;
+            scanf("%d", &h);
+
+            if(h <= max && h >= min){
+                cont++;
+            }
+        }
+
+        printf("%d\n", cont);
+    }
+    return 0;
+}
