38bdfa1e2c6e6a639d39621ce03ca65af79e51b7
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2022-12-30 22:07:19 -0300

    Question 2581

---
+++ Beginner/2581.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    char quest[1000];
+    while(t--){
+        char s;
+        scanf("%c", &s);
+        scanf("%[^\n\r]", quest);
+        printf("I am Toorg!\n");
+    }
+
+    return 0;
+}
