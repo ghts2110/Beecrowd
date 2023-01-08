e85bf9978a7176a4818917979a602ff91dc0f185
G4br13l-T0rr3s <59540600+ghts2110@users.noreply.github.com>
2023-01-07 23:26:33 -0300

    Theon's Answer

--- Beginner/1858.c
+++ Beginner/1858.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d", &t);
     while(t--){
         scanf("%d", &n);
-        if(n < min){
+        if(n < min){ 
             bestIndex = index;
             min = n;
         }
