ebbdccbb068499cf85c1898a618a8c26c71d3b25
G4br13l-T0rr3s <59540600+ghts2110@users.noreply.github.com>
2023-01-07 23:30:33 -0300

    Regular Simple Polygons

--- Beginner/1959.c
+++ Beginner/1959.c
@@ -3,7 +3,7 @@
 int main(){
     long long int N, L;
     scanf("%lld %lld", &N, &L);
-    long long int value = N * L;
+    long long int value = N * L; 
     printf("%lld\n", value);
     return 0;
 }
