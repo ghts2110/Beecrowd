87f73283da1e0d125979708826943629a9d54bfc
G4br13l-T0rr3s <59540600+ghts2110@users.noreply.github.com>
2023-01-07 23:23:52 -0300

    Bob Conduit

--- Beginner/1589.c
+++ Beginner/1589.c
@@ -4,7 +4,7 @@ int main(){
     int t, r1, r2;
     scanf("%d", &t);
     
-    while(t--){
+    while(t--){ 
         scanf("%d %d", &r1, &r2);
         int r3 = r1 + r2;
         printf("%d\n", r3);
