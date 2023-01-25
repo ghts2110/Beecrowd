9ab856f05aa36be4f9389f9a2d676a2a8b9ce738
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-01-25 14:58:57 -0300

    Average 1

---
+++ Beginner/1005.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        
+        System.out.printf("MEDIA = %.5f\n", ((a * 3.5) + (b * 7.5))/11);
+    }
+}
