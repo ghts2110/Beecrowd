ba47f30ff5972f6ff78499142cabf85f450b417a
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-01-25 14:52:00 -0300

    Average 1

--- Beginner/1002.java
+++ Beginner/1002.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        double r;
+        double a, b;
         Scanner sc = new Scanner(System.in);
         
-        r = sc.nextDouble();
-        System.out.printf("A=%.4f\n", r * r * 3.14159);
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        
+        System.out.printf("A=%.5f\n", ((a * 3.5) + (b * 7.5))/11)5,5;
     }
 }
---
+++ Beginner/1005.py
@@ -0,0 +1,6 @@
+if '__Main__':
+    a = float(input())
+    b = float(input())
+
+    value = ((a * 3.5)+(b * 7.5))/11
+    print(f"MEDIA = {value:.5f}")
